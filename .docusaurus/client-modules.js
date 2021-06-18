c0657e10ef7651215b0730c4960767d794306737
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-06-18 04:01:46 -0300

    Configurando

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/roberto/Documentos/sites/novo/doc-test/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/roberto/Documentos/sites/novo/doc-test/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/roberto/Documentos/sites/novo/doc-test/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/roberto/Documentos/sites/novo/doc-test/node_modules/infima/dist/css/default/default.css"),
-  require("/home/roberto/Documentos/sites/novo/doc-test/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/roberto/Documentos/sites/novo/doc-test/src/css/custom.css"),
-];
