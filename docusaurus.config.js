3481bea75a7f9fd1f28043e15c18a0377ccb7dac
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-06-18 06:18:27 -0300

    add version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ module.exports = {
           position: 'left',
           label: 'Docs',
         },
+	{type: 'docsVersionDropdown',},
 	{
           href: 'https://github.com/HashLDash/Photon',
           label: 'GitHub',
